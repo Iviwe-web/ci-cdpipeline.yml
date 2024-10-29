974a82bb0d0563a9aa74b34aeb079539f1cc68dd
IviweFunda <IviweFunda@CTUTRAINING1035>
2024-10-29 13:47:47 +0200

    Add project files.

---
+++ ci-cdpipeline.yml/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ci_cdpipeline.yml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
